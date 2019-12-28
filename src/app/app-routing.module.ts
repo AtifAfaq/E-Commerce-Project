19db0331308bb42942454bb542a2b5419e79e2b1
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2019-12-28 21:57:34 +0500

    Porduct Item UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {SignupComponent} from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [ 
+const routes: Routes = [
   { path: 'login', component: LoginComponent },
-{ path: 'signup', component: SignupComponent },
-{path: 'home', component: HomeComponent },
-{ path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(
- 
-  routes,
+
+    routes,
     { enableTracing: true } // <-- debugging purposes only
   )],
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
