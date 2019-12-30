9d487ba0ba4161bee127140149c3a400441bfd3b
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2019-12-30 22:55:27 +0500

    Product detail in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'eCommerceProject';
+
+  onActivate(event) {
+    window.scroll(0, 0);
+  }
 }
