25bea6565a6e6ec7a2fa45140d9207e8746c5eb1
Muhammad Afaq <43587275+AtifAfaq@users.noreply.github.com>
2019-12-31 11:02:03 -0500

    UI of Product Details Component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-
+value:number;
   constructor() { }
 
   ngOnInit() {
+  }
+  increaseValue() {
+    // var value = parseInt(document.getElementById('number').value, 10);
+    // value = isNaN(value) ? 0 : value;
+    // value++;
+    // document.getElementById('number').value = value;
+  }
+decreaseValue() {
+    // var value = parseInt(document.getElementById('number').value, 10);
+    // value = isNaN(value) ? 0 : value;
+    // value < 1 ? value = 1 : '';
+    // value--;
+    // document.getElementById('number').value = value;
   }
 
 }
