c363d91e55dea80e10726e9bd796b3c8cdfba8fc
Muhammad Afaq <43587275+AtifAfaq@users.noreply.github.com>
2020-01-03 10:10:14 -0500

    Seller home page defined

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,7 +13,8 @@ export class NavbarComponent implements OnInit {
   constructor(
     public router: Router
   ) {
-    this.userType = localStorage.getItem('userType') || 'buyer';
+    // if there is no user type then go to buyer
+    this.userType = localStorage.getItem('userType') || 'buyer'; 
   }
 
   ngOnInit() {
