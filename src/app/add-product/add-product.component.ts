03f89f72ec04f0dba65109610b36116dd37ca5a6
Muhammad Afaq <43587275+AtifAfaq@users.noreply.github.com>
2020-01-04 22:58:25 -0500

    Add product Component

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
-
+  imagePath : any = {};
+  imgURL:string = '';
+  message: any = '';
   constructor() { }
-
+  
   ngOnInit() {
   }
+preview(files) {
+    this.message = "";
+    if (files.length === 0)
+      return;
 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
+  }
 }
