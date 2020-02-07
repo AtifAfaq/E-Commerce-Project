7c004af0bd9aa9fc9c664c7cd9a00720d83dea7d
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-02-07 23:51:34 +0500

    Add to cart

--- src/app/data-collector.service.ts
+++ src/app/data-collector.service.ts
@@ -4,23 +4,24 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class DataCollectorService {
+
   myArray: any = [];
   product: any = {};
   isEdit: boolean;
   productQty: number;
+
   constructor(public router: Router) {
 
   }
+
+
   AddtoCart() {
     var retreivedProducts = localStorage.getItem("products");
     this.myArray = JSON.parse(retreivedProducts);
     if (!this.myArray) {
-      debugger;
       this.myArray = [];
     }
-    this.product.productQty = this.productQty;
     this.myArray.push(this.product);
-    debugger;
     localStorage.setItem("products", JSON.stringify(this.myArray));
     this.router.navigate(["/cart"]);
     console.log(this.myArray);
