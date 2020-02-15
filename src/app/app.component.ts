651cfba98a989613933cdea3cca03b95ef52d7be
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-02-16 00:05:08 +0500

    Modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,24 @@ export class AppComponent {
   onActivate(event) {
     window.scroll(0, 0);
   }
+
+
+  // hello() {
+  //   var self = this;
+  //   for (var i = 0; i < self.myArray.length; i++) {
+  //     firebase.database().ref().child('products/' + self.myArray[i].key)
+  //       .once('value', (snapshot) => {
+  //         var data = snapshot.val();
+  //         data.availableQty = data.availableQty - self.array[i].qty;
+  //         var updates = {};
+  //         updates['/products/' + key + '/' + 'availableQty'] = data.availableQty;
+  //         firebase.database().ref().update(updates)
+  //           .then(() => {
+
+  //           })
+  //       })
+  //   }
+  // }
+
+
 }
