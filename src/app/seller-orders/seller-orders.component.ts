785ca36ac5da6dd708a06a0c7e20c0c2d54034e8
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-02-19 23:25:55 +0500

    Seller orders UI

--- src/app/seller-orders/seller-orders.component.ts
+++ src/app/seller-orders/seller-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-seller-orders',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerOrdersComponent implements OnInit {
 
+  myOrders: Array<any> = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.getMyOrders();
+  }
+
+
+  getMyOrders() {
+    var self = this;
+    firebase.database().ref().child('orders')
+      .once('value', (snapshot) => {
+        var data = snapshot.val();   //  JSON objects array
+        for (var key in data) {
+          var temp = data[key];     //   {name: 'Atif', city: 'lahore'} 
+          temp.key = key;           //   {name: 'Atif', key: 'asjdUIH32jnUYHIK8'} 
+          for (var i = 0; i < temp.myArray.length; i++) {
+            if (temp.myArray[i].uid == localStorage.getItem('uid')) {
+              self.myOrders.push(temp);
+            }
+          }
+        }
+        console.log(self.myOrders);
+      })
   }
 
 }
