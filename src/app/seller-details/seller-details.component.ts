f6def80d27f52248a205bc5a22b8d90928583d9e
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-02-22 23:46:22 +0500

    Order accept/cancel

--- src/app/seller-details/seller-details.component.ts
+++ src/app/seller-details/seller-details.component.ts
@@ -8,9 +8,11 @@ import * as firebase from 'firebase';
   styleUrls: ['./seller-details.component.scss']
 })
 export class SellerDetailsComponent implements OnInit {
+
   key: string = '';
   order: any = {};
   orderProducts = [];
+  allProducts = [];
   loading = false;
   subTotal;
   shipmentCharges;
@@ -24,16 +26,17 @@ export class SellerDetailsComponent implements OnInit {
   ngOnInit() {
 
   }
+
+
   getUserDetail(key) {
     firebase.database().ref().child('orders/' + key)
       .once('value', (snapshot) => {
         this.order = snapshot.val();
+        this.allProducts = this.order.myArray;
         this.order.myArray.forEach(product => {
           let uid = localStorage.getItem('uid');
           if (uid == product.uid) {
             this.orderProducts.push(product)
-            console.log(this.orderProducts)
-
           }
         })
         this.getTotalCost()
@@ -41,8 +44,8 @@ export class SellerDetailsComponent implements OnInit {
       .catch((e) => {
         alert(e.message);
       })
-
   }
+
 
   GrandTotal(p) {
     var totalPrice = Number(p.productQty) * Number(p.discountedPrice);
@@ -62,92 +65,25 @@ export class SellerDetailsComponent implements OnInit {
   }
 
 
-  accept(p) {
-    p.status = "accepted";
-    var updates = {};
-    var postKey = firebase.database().ref().child('orders').push().key;
-    debugger;
-    updates['/orders/' + postKey] = p.status;
-    firebase.database().ref().update(updates)
-      .then(() => {
-        alert("product accepted")
-      })
-      .catch((e) => {
-        this.loading = false;
-        alert(e.message);
-      })
+  acceptOrder(p, index, status) {
+    var self = this;
+    self.allProducts.forEach(product => {
+      if (product.key == p.key) {
+        product.status = status;
+        self.updateStatus(index, status);
+      }
+    });
   }
 
 
-
-
-  //   placeOrder() {
-  //     var userData: any = {
-  //       firstName: this.firstName,
-  //       lastName: this.lastName,
-  //       email: this.email,
-  //       address: this.address,
-  //       uid: localStorage.getItem('uid'),
-  //       myArray: this.myArray,
-  //       totalBill: this.totalBill,
-  //       shipmentCharges: this.shipmentCharges,
-  //       subTotal: this.subTotal,
-  //       status: this.status,
-  //       timestamp: Number(new Date())
-  //     }
-  //     var updates = {};
-  //     var postKey = firebase.database().ref().child('orders').push().key;
-  //     updates['/orders/' + postKey] = userData;
-  //     firebase.database().ref().update(updates)
-  //       .then(() => {
-  //         for (var i = 0; i < this.myArray.length; i++) {
-  //           this.zone.run(() => {
-  //             this.updateQty(i);
-  //           })
-  //         }
-  //       })
-  //       .catch((e) => {
-  //         this.loading = false;
-  //         alert(e.message);
-  //       })
-  //   }
-
-
-  //   updateQty(i) {
-  //     var self = this;
-  //     firebase.database().ref().child('products/' + self.myArray[i].key)
-  //       .once('value', (snapshot) => {
-  //         var data = snapshot.val();
-  //         data.availableQty = Number(data.availableQty) - Number(self.myArray[i].productQty);
-  //         var updates = {};
-  //         updates['/products/' + self.myArray[i].key + '/' + 'availableQty'] = data.availableQty;
-  //         firebase.database().ref().update(updates)
-  //           .then(() => {
-  //             if (i >= (this.myArray.length - 1)) {
-
-  //               this.loading = false;
-  //               var retreivedProducts = localStorage.getItem("products");
-  //               this.myArray = JSON.parse(retreivedProducts);
-  //               this.myArray = [];
-  //               localStorage.setItem("products", JSON.stringify(this.myArray));
-  //               this.totalBill = 0;
-  //               localStorage.setItem("totalBill", JSON.stringify(this.totalBill));
-  //               this.shipmentCharges = 0;
-  //               localStorage.setItem("shipmentCharges", JSON.stringify(this.shipmentCharges));
-  //               this.subTotal = 0;
-  //               localStorage.setItem("subTotal", JSON.stringify(this.subTotal));
-  //               this.service.getCartCount();
-  //               alert("Your order has been placed!");
-  //               this.router.navigate(['/myorders']);
-  //             }
-  //           })
-  //       })
-  //   }
-
-  // }
-
-
-
+  updateStatus(index, status) {
+    var self = this;
+    var updates = {};
+    updates['/orders/' + self.key + '/myArray'] = self.allProducts;
+    firebase.database().ref().update(updates).then(() => {
+      self.orderProducts[index].status = status;
+    })
+  }
 
 
 }
