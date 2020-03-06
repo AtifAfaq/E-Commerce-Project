f5cc57ec316e7e2e1d007b4af3deac5c973e6c67
Afaq, Muhammad <muhammad.afaq@accenture.com>
2020-03-06 11:09:46 -0500

    Review section update

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -13,7 +13,9 @@ export class DetailsComponent implements OnInit {
   myArray: any = [];
   allProducts = [];
   orderObj: any = {};
-
+  comment: '';
+  loading;
+  productKey;
   constructor(public router: Router,
     public service: DataCollectorService) {
 
@@ -45,6 +47,52 @@ export class DetailsComponent implements OnInit {
         }
       })
   }
+
+  reviewDataFirebase() {
+    var review: any = {
+      comment: this.comment,
+      firstName: localStorage.getItem('firstName'),
+      lastName: localStorage.getItem('lastName'),
+      email: localStorage.getItem('email'),
+      uid: localStorage.getItem('uid'),
+      productKey: this.productKey,
+      timestamp: Number(new Date())
+    }
+    this.loading = true;
+    var updates = {};
+    var postKey = firebase.database().ref().child('reviews').push().key;
+    updates['/reviews/' + postKey] = review;
+    firebase.database().ref().update(updates)
+      .then(() => {
+        this.loading = false;
+      })
+      .catch((e) => {
+        this.loading = false;
+        alert(e.message);
+      })
+  }
+
+  sendProduct(key) {
+    this.productKey = key;
+  }
+  // updates['/users/' + this.uid] = userData;
+  // firebase.database().ref().update(updates)
+  //   .then(() => {
+  //     alert("Your account has been successfully created!");
+  //     localStorage.setItem('firstName', this.firstName);
+  //     localStorage.setItem('lastName', this.lastName);
+  //     localStorage.setItem('email', this.email);
+  //     localStorage.setItem('uid', this.uid);
+  //     localStorage.setItem('userLoggedIn', 'true');
+  //     this.loading = false;
+  //     firebase.auth().currentUser.sendEmailVerification();
+  //     this.router.navigate(['/home']);
+  //   })
+  //   .catch((e) => {
+  //     this.loading = false;
+  //     alert(e.message);
+  //   })
+
 
 }
 
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataCollectorService } from './../data-collector.service';
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,13 +11,17 @@ export class ProductDetailsComponent implements OnInit {
 
   productQty: number = 1;
   product: any = {}
+  loading = false;
+  myReview: any = [];
+  myReviewUser: any = [];
 
   constructor(public service: DataCollectorService) {
     this.product = this.service.product
+    console.log(this.product)
   }
 
   ngOnInit() {
-
+    this.getMyReviews()
   }
 
 
@@ -48,6 +54,38 @@ export class ProductDetailsComponent implements OnInit {
       alert("Product is not in stock")
     }
 
+  }
+
+  getMyReviews() {
+    var self = this;
+    self.loading = true;
+
+    debugger;
+    firebase.database().ref().child('reviews')
+      .orderByChild('productKey').equalTo(this.product.key)
+      .once('value', (snapshot) => {
+        var data = snapshot.val();
+        for (var key in data) {
+          var review = data[key];
+          var myuid = review.uid;
+          self.myReview.push(review);
+        }
+        console.log(self.myReview);
+        self.loading = false;
+        firebase.database().ref().child('users')
+          .orderByChild('uid').equalTo(myuid)
+          .once('value', (snapshot) => {
+            var data = snapshot.val();
+
+            for (var key in data) {
+              var user = data[key];
+              // temp.key = key;
+              self.myReviewUser.push(user);
+            }
+            console.log(self.myReviewUser);
+            self.loading = false;
+          })
+      })
   }
 
 }
